857f3388da0b3dac8b19310356022ce914f57553
kris j <kris.jurkans@gmail.com>
2023-12-03 16:53:41 +0000

    updated testbench, now can execute long division program

--- tb/tb_manchester_baby_ctb.v
+++ tb/tb_manchester_baby_ctb.v
@@ -1,26 +1,26 @@
 module tb_manchester_baby (
     input wire clock,
     input wire reset_i,
-    inout [31:0] ram_data_io,
+    input [31:0] ram_data_i,
+    output [31:0] ram_data_o,
     output wire [4:0] ram_addr_o,
     output wire ram_rw_en_o,
     output wire stop_lamp_o,
-    output wire logisim_clock_tree_0_out,
-    output wire logisim_clock_tree_3_out
+    output wire logisim_clock_tree_0_out
 );
 
     `ifdef COCOTB_SIM
         initial begin
-            $dumpfile("waves_tb_manchester_baby.vcd");
+            $dumpfile("waves_ctb_tb_manchester_baby.vcd");
             $dumpvars(0, uut_manchester_baby);
         end
     `endif
 
-    openlane_manchester_baby uut_manchester_baby (
-        .clock(clock), .reset_i(reset_i), .ram_data_io(ram_data_io),
-        .ram_addr_o(ram_addr_o), .ram_rw_en_o(ram_rw_en_o), .stop_lamp_o(stop_lamp_o),
-        .logisim_clock_tree_0_out(logisim_clock_tree_0_out), 
-        .logisim_clock_tree_3_out(logisim_clock_tree_3_out)
+    manchester_baby uut_manchester_baby (
+        .clock(clock), .reset_i(reset_i), .ram_data_i(ram_data_i),
+        .ram_data_o(ram_data_o), .ram_addr_o(ram_addr_o), 
+        .ram_rw_en_o(ram_rw_en_o), .stop_lamp_o(stop_lamp_o),
+        .logisim_clock_tree_0_out(logisim_clock_tree_0_out)
     );
 
 endmodule
